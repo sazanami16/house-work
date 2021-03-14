7a19ee67e820e1973022a1f5128e6f6a5ada3cf8
takuto <kaza.solution95@gmail.com>
2021-03-15 00:48:34 +0900

    bad_nice migration設定

--- db/migrate/20210314153958_create_nices.rb
+++ db/migrate/20210314153958_create_nices.rb
@@ -1,7 +1,8 @@
 class CreateNices < ActiveRecord::Migration[6.0]
   def change
     create_table :nices do |t|
-
+      t.references :user, null:false
+      t.references :report, null:false
       t.timestamps
     end
   end
--- db/migrate/20210314154005_create_bads.rb
+++ db/migrate/20210314154005_create_bads.rb
@@ -1,7 +1,8 @@
 class CreateBads < ActiveRecord::Migration[6.0]
   def change
     create_table :bads do |t|
-
+      t.references :user, null:false
+      t.references :report, null:false
       t.timestamps
     end
   end
