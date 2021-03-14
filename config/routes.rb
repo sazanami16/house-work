d3ae97dd7d6162883a5b960b74fdbd12584ec2c0
takuto <kaza.solution95@gmail.com>
2021-03-15 00:25:54 +0900

    route追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "works#index"
+  resources :works, only: [:index]
+  resources :reports, only: [:create]
+  resources :nices, only: [:create, :destroy]
+  resources :bads, only: [:create, :destroy]
 end
