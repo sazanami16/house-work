db002c1c2234fe23015a825abac927954c9d793d
takuto <kaza.solution95@gmail.com>
2021-03-15 00:25:08 +0900

    work_controller追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require('jquery')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -15,3 +16,10 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+//= require rails-ujs
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require bootstrap.min
+//= require_tree
