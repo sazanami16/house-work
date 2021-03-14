13410d25cd6fcb3f4c3b3883be366b55b920b6b8
takuto <kaza.solution95@gmail.com>
2021-03-15 00:47:33 +0900

    report_migration設定

--- db/migrate/20210314153952_create_reports.rb
+++ db/migrate/20210314153952_create_reports.rb
@@ -1,7 +1,9 @@
 class CreateReports < ActiveRecord::Migration[6.0]
   def change
     create_table :reports do |t|
-
+      t.string :message, null:false
+      t.references :user, null:false
+      t.references :work, null:false
       t.timestamps
     end
   end
