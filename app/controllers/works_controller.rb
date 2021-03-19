07a0b5efd0a4738ce8b3b1065410fbb58ca4bd04
kodaikawabe <kokodai5124@icloud.com>
2021-03-19 19:52:48 +0900

    newアクション、createアクション定義

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,24 @@
 class WorksController < ApplicationController
+
+  def new
+    @work = work.new
+  end
+
+  def create
+    @work = Work.new(work_params)
+    if @work.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+
+
+  private
+
+  def work_params
+    params.require(:work).permit(:name, :rule, :weight).merge(user_id: current_user.id)
+  end
 end
