449f9a5056a81e506a6a8775cb2b46480609d9a3
kodaikawabe <kokodai5124@icloud.com>
2021-03-20 23:38:03 +0900

    html,css編集

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,5 +1,7 @@
 class Work < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :weight
   validates :name, presence: true
-  validates :weight, presence: true
+  validates :weight_id, numericality: { other_than: 1 }
   validates :rule, presence: true
 end
