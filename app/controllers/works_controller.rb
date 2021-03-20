4cd5979f09717b241269e0632f8d3e7f1eba4562
kodaikawabe <kokodai5124@icloud.com>
2021-03-20 23:36:28 +0900

    コントローラー修正

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -21,12 +21,35 @@ class WorksController < ApplicationController
     @work = Work.find(params[:id])
   end
 
+  def edit
+    @work = Work.find(params[:id])
+  end
+
+  def update
+    @work = Work.find(params[:id])
+    if @work.update(work_params)
+      redirect_to work_path(@work.id)
+    else
+      render :edit
+    end
+  end
+  
+
+  def destroy
+    work = Work.find(params[:id])
+    if work.destroy
+      redirect_to root_path
+    else
+      render work_path(work.id)
+    end
+  end
+
 
 
 
   private
 
   def work_params
-    params.require(:work).permit(:name, :weight, :rule)
+    params.require(:work).permit(:name, :weight_id, :rule)
   end
 end
