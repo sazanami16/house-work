e7d7e81b6aac08066ad067e3549f80be918612c4
masanaring <73056512+masanaring@users.noreply.github.com>
2021-03-25 09:43:14 +0900

    消耗品一覧表示及び削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,20 +1,34 @@
 class ItemsController < ApplicationController
+  before_action :find_params, only: [:show, :edit, :update, :destroy]
+
   def new
     @item = Item.new
   end
 
   def create
-    @item = Item.new(item_params)
+   @item = Item.new(item_params)
     if @item.save
-      redirect_to root_path
+      redirect_to "/works/#{@item.work.id}"
     else
       render :new
     end
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to "/works/#{@item.work.id}"
   end
 
   private
 
   def item_params
     params.require(:item).permit(:name,:quantity,:need_id).merge(work_id: params[:work_id])
+  end
+  def work_params
+    params.require(:work).permit(:name, :weight_id, :rule)
+  end
+  def find_params
+    @item = Item.find(params[:id])
   end
 end
