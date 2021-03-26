9a11c247d14c5801b881db7bda4fa8978f11146a
takuto <kaza.solution95@gmail.com>
2021-03-27 02:06:35 +0900

    seed追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,14 @@ Work.create do |a|
   a.id = 1
   a.name = '洗濯'
   a.rule = '柔軟剤を使う'
+  a.weight_id = 3
+  a.user_id = 1
+
+  a.id = 2
+  a.name = '洗濯aa'
+  a.rule = '柔軟剤を使うaa'
+  a.weight_id = 3
+  a.user_id = 1
+
 end
 
-Report.create do |a|
-  a.id = 1
-  a.message = 'ガチでやった'
-end
-
-User.create do |a|
-  a.id = 1
-  a.name = '田中'
-end
